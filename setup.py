dbf2dbfbf1355b48eb80b14746448bcd49afd6a7
SUNDAR <aaransoftware@gmail.com>
2025-06-29 18:29:04 +0530

    #10 removing emoji

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="prefiq",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "click>=8.0.0",
         "rich>=13.0.0"
@@ -11,6 +12,12 @@ setup(
     entry_points={
         "console_scripts": [
             "prefiq=prefiq.cli:cli",
+        ],
+    },
+    package_data={
+        "prefiq": [
+            "templates/prefentity/*",
+            "templates/app_full/*",
         ],
     },
 )
